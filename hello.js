c3412834d664c413c0f20be675ae889fd40098a8
Noel Ferreira <noel.ferreira@gmail.com>
2015-03-30 16:25:55 -0700

    Add js file and trying to use it

---
+++ hello.js
@@ -0,0 +1,19 @@
+function renderInfo(){
+    var state = wave.getState();
+    var html = "";
+    html += '<div style="width: 50px; height:50px; margin-left: 10px; margin-top: 10px; background-color: red;">';
+    document.getElementById('helloDiv').innerHTML = html;
+}
+
+function init() {
+    if (wave && wave.isInWaveContainer()) {
+        // Loads the gadget's initial state and the subsequent changes to it
+        wave.setStateCallback(renderInfo);
+        
+        // Loads participants and any changes to them
+        wave.setParticipantCallback(renderInfo);
+    }
+}
+
+// Initializes gadget after receiving a notification that the page is loaded and the DOM is ready.
+gadgets.util.registerOnLoadHandler(init);
